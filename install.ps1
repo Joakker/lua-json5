7e27015796be2de32272c0725639561a1ac173fb
Maneren <maneren731@gmail.com>
2025-09-29 22:14:24 +0200

    Add support for CARGO_TARGET_DIR

--- install.ps1
+++ install.ps1
@@ -1,2 +1,4 @@
-cargo build --features luajit --release
-mv .\target\release\lua_json5.dll lua\json5.dll
+$TARGET_DIR = if ($env:CARGO_TARGET_DIR) { $env:CARGO_TARGET_DIR } else { "target" }
+
+cargo build --features luajit --release --target-dir "$TARGET_DIR"
+Move-Item -Path "$TARGET_DIR\release\lua_json5.dll" -Destination lua\json5.dll
--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
-cargo build --features luajit --release --target-dir ./target
+TARGET_DIR=${CARGO_TARGET_DIR:-target}
+
+cargo build --features luajit --release --target-dir "$TARGET_DIR"
 
 case $OSTYPE in
 "linux-gnu"*)
-	mv ./target/release/liblua_json5.so lua/json5.so
+	mv "$TARGET_DIR"/release/liblua_json5.so lua/json5.so
 	strip lua/json5.so
 	;;
 "darwin"*)
 	# Provide both just in case
-	cp ./target/release/liblua_json5.dylib lua/json5.dylib
+	cp "$TARGET_DIR"/release/liblua_json5.dylib lua/json5.dylib
 	cp lua/json5.dylib lua/json5.so
 	;;
 esac
